116a3e91bc3ac60a4ffe3b9b4b993c70e17c50ec
joshi-durgesh <durgesh7joshi9@gmail.com>
2024-01-31 22:35:46 +0530

    fetch data from server

--- src/component/TopBrandsSection.js
+++ src/component/TopBrandsSection.js
@@ -1,9 +1,23 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
-import TopBrandsData from "../utils/TopBrandsData";
+import { useState, useEffect } from "react";
 
 const TopBrandsSection = () => {
-  const [TopBrands, SetTopBrands] = useState(TopBrandsData);
+  const [TopBrands, SetTopBrands] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.487739974955158&lng=78.38145008147583&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    SetTopBrands(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
   return (
     <section className='top-brand-section'>
       <div className='about-banner'>
